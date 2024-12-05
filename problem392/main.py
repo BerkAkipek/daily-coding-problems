63beee42cf7f4a62df8470ffbaf9ac3ce5e14c18
BerkAkipek <berk.akipek.96@gmail.com>
2024-12-05 18:36:40 +0300

    Solution added to problem 392

--- problem392/main.py
+++ problem392/main.py
@@ -19,10 +19,40 @@
 [0, 0, 1, 0]]
 # Return 14.
 
-def island_perimeter(matrix):
-    surfaces = 0
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            if matrix[i][j] == 1:
-                # Land
-                pass
+## Solution ##
+
+# When counting the perimeter of an island here, notice that a straightforward way to check whether an edge should be counted as part of the perimeter is whether it is adjacent to another land cell. 
+# If there's a land cell adjacent to that edge, then it shouldn't count as the perimeter. 
+# If it's a water cell or an edge, then it should count as the perimeter.
+
+# This leads us to the following algorithm to compute the perimeter:
+# 1) Iterate over every cell in the board 
+# 2) Count the number of neighbouring land cells 
+# 3) Add 4 - num_land_cells to the perimeter count
+
+def get_num_neighbours(board, r, c):
+    num = 0
+    if r > 0:
+        num += board[r-1][c] == 1
+    
+    if r < len(board) - 1:
+        num += board[r+1][c] == 1
+    
+    if c > 0:
+        num += board[r][c-1] == 1
+    
+    if c < len(board[0]) - 1:
+        num += board[r][c+1] == 1
+    
+    return num
+
+
+def island_perimeter(board):
+    perimeter = 0
+    for r, row in enumerate(board):
+        for c, val in enumerate(row):
+            if val == 1:
+                perimeter += 4 - get_num_neighbours(board, r, c)
+    return perimeter
+
+# This runs in O(M N) time and constant space, since computing neighbours is constant time and we iterate over every cell on the N M board.
